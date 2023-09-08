57f11e72532834be06ac1382b7cdb174ccbd92c4
Muhammad-Farasat <139209158+Muhammad-Farasat@users.noreply.github.com>
2023-09-08 19:26:25 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,134 @@
+//!  Question no.1
+
+//? var num1 = +prompt('Enter first  Number')
+//? var num2 = +prompt('Enter second number')
+//? var result = num1+num2
+//? console.log(result)
+//? document.write(result)
+
+//! Question no.2
+
+//? var num1 = +prompt('Enter first  Number')
+//? var num2 = +prompt('Enter second number')
+//? var addition = num1+num2
+//? var substraction = num1-num2
+//? var dividion = num1/num2
+//? var reminder = num1%num2
+//? console.log(addition,substraction,dividion,reminder)
+//? document.write("This is add:"+addition,"This is sub:"+substraction,"This is div:"+dividion,"This is module:"+reminder)
+
+//! Question no.3
+
+//? var num1
+//? document.write("Value after decleration is:"+num1+"<br>")
+//? var num2 = 5
+//? document.write("Initial value is:"+num2+"<br>")
+//? var newNum = ++num2
+//? document.write("Value after increment:"+newNum+"<br>")
+//? var num3 = 7
+//? var result = num3 + num2
+//? document.write("After Addition:"+result+"<br>")
+//? var dec = --result
+//? document.write("After decrement:"+dec+"<br>")
+//? var rem = dec % 3
+//? document.write("The reminder is:"+rem+"<br>")
+
+//! Question no.4
+
+//? var ticket = 600
+//? var cost = ticket * 5
+//? document.write("The cost of 5 ticket is:"+cost)
+
+//! Queston no.5
+
+//? var table = prompt("Enter any no for table")
+//? var table1 = table * 1
+//? var table2 = table * 2
+//? var table3 = table * 3
+//? var table4 = table * 4
+//? var table5 = table * 5
+//? var table6 = table * 6
+//? var table7 = table * 7
+//? var table8 = table * 8
+//? var table9 = table * 9
+//? var table10 = table * 10
+//?document.write("The table of:"+table+"<br>")
+//? document.write(table +"x 1 ="+table1+"<br>")
+//? document.write(table +"x 2 ="+table2+"<br>")
+//? document.write(table +"x 3 ="+table3+"<br>")
+//? document.write(table +"x 4 ="+table4+"<br>")
+//? document.write(table +"x 5 ="+table5+"<br>")
+//? document.write(table +"x 6 ="+table6+"<br>")
+//? document.write(table +"x 7 ="+table7+"<br>")
+//? document.write(table +"x 8 ="+table8+"<br>")
+//? document.write(table +"x 9 ="+table9+"<br>")
+//? document.write(table +"x 10 ="+table10+"<br>")
+
+//! Question no.6
+
+//? var item1 = +prompt("Enter price 1")
+//? var item2 = +prompt("Enter price 2")
+//? var item3 = +prompt("Enter quantity 1")
+//? var item4 = +prompt("Enter quantity 2")
+//? var item5 = +prompt("Enter shipping")
+//? var total = item1+item2+item3+item4+item5
+
+//? document.write("Price of item 1"+item1+"<br>")
+//? document.write("Price of item 2"+item2+"<br>")
+//? document.write("Ordered quantity of item 1"+item3+"<br>")
+//? document.write("Ordered Quantity of item 2"+item4+"<br>")
+//? document.write("Shipping charges"+item5+"<br>")
+//? document.write("Total cost of order is:"+total)
+
+//! Question no.7
+
+//? var total = +prompt("Enter total no")
+//? var obtain = +prompt("Enter obtained no")
+//? var percent = (total*100)/obtain
+
+//? document.write("Total Marks is:"+total+"<br>")
+//? document.write("Obtained Marks is:"+obtain+"<br>")
+//? document.write("The percentage is:"+percent+"<br>")
+
+//! Question no.8
+
+//? var dollar = 306.27
+//? var riyal = 81.27
+
+//? document.write("Total Currancy: " +(10*dollar)+(25*riyal))
+
+//! Question no.9
+
+//? var calcul = 5
+
+//? document.write("Add 5: "+ (calcul+5)+"<br>"+"Multiply by 10: "+(calcul-10)+"<br>"+"Divide by 2: "+(calcul/2))
+
+//! Question no.10
+
+//? var currDate = 2023
+//? var birth = 2005
+//? var age = currDate-birth
+
+//? document.write("Your age is: " + age )
+
+//! Question no.11
+
+//? var radius = 20
+//? var circum = 2*3.147*radius
+//? var area = 3.147*radius*radius
+
+//? document.write("The radius is: " + radius+"<br>" )
+//? document.write("The circum is: " + circum+"<br>" )
+//? document.write("The area of a circle is: " + area+"<br>" )
+
+//! Question no.12
+
+
+var snack = "burger"
+var age = 18
+var maxAge = 60
+var amount = 3
+
+var total = (maxAge-age)*amount
+
+document.write("You will need"+ total +"to last you until the ripe old age of" + maxAge )
